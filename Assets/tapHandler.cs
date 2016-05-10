bb112c24bdbe82b0e470baf410c3d7ba89b95013
Clement Barbisan <c.barbisan@free.fr>
2016-05-10 21:54:07 +0200

    initial commit

---
+++ Assets/tapHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tapHandler : MonoBehaviour {
+	GameObject[] list;
+	int index;
+
+	// Use this for initialization
+	void Start () {
+		index = 0;
+		list = GameObject.FindGameObjectsWithTag ("model1");
+		for (int i = 0; i < list.Length; i++) 
+		{
+			if (i == index)
+				list [i].SetActive(true);
+			else
+				list [i].SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			index = (index + 1) % list.Length;
+			for (int i = 0; i < list.Length; i++) 
+			{
+				if (i == index)
+					list [i].SetActive(true);
+				else
+					list [i].SetActive(false);
+			}
+		}
+	}
+}
